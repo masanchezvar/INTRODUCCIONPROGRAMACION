0f941876ac1c83ebff7b7743657b34b6d6452e01
masanchezvar <142190930+masanchezvar@users.noreply.github.com>
2023-09-04 22:49:10 -0600

    Add files via upload

---
+++ SEMANA5_LABORATORIO_1055323_ALEXANDRASÁNCHEZ/L1MASV1055323/L1MASV1055323/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        /*
+        Console.WriteLine("Hola mundo");
+        Console.WriteLine("Soy Alexandra");
+
+        Console.Write("Hola mundo");
+        Console.Write(" Soy Alexandra"); */
+
+        /*comentarios programa*/
+        /*Console.ReadKey();
+        Console.ReadLine();*/
+
+        Console.WriteLine("Ingrese su nombre:");
+        string Nombre = Console.ReadLine();
+
+        Console.WriteLine("Hola mundo");
+        Console.WriteLine("soy " + Nombre);
+
+        Console.Write("Hola mundo ");
+        Console.Write("soy " + Nombre);
+        Console.ReadKey();
+    }
+}
---
+++ SEMANA5_LABORATORIO_1055323_ALEXANDRASÁNCHEZ/T1MASV1055323/T1MASV1055323/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Mi segundo programa");
+
+        Console.WriteLine("Ingrese su nombre: ");
+        string Nombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su edad: ");
+        string Edad = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su carrera: ");
+        string Carrera = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su carné: ");
+        string Carne = Console.ReadLine();
+
+        Console.WriteLine("Mi segundo programa");
+        Console.WriteLine("Nombre: " + Nombre);
+        Console.WriteLine("Edad: " + Edad);
+        Console.WriteLine("Carrera: " + Carrera);
+        Console.WriteLine("Carné: " + Carne);
+
+        Console.WriteLine("Soy " + Nombre + ", tengo " + Edad + " años y estudio la carrera de " + Carrera + ". Mi número de carné es; " + Carne);
+
+        Console.ReadKey();
+    }
+}
