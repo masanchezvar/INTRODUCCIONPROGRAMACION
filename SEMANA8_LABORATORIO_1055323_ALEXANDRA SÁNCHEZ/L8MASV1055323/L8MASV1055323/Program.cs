d7b019911f1d4e516e689371422dfc24aaae1093
masanchezvar <142190930+masanchezvar@users.noreply.github.com>
2023-09-25 10:03:33 -0600

    Add files via upload

---
+++ SEMANA8_LABORATORIO_1055323_ALEXANDRA SÁNCHEZ/L8MASV1055323/L8MASV1055323/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Laboratorio semana 8");
+        Console.WriteLine("\nIngresar un número");
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string resultado = "";
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        int i = 2;
+
+
+        if (n > 0)
+        {
+            resultado = a.ToString();
+            Console.WriteLine("Resultado = " + a);
+            if (n > 1)
+            {
+                resultado += b.ToString();
+
+                while (i < n)
+                {
+                    c = a + b;
+                    resultado += c.ToString();
+                    a = b;
+                    b = c;
+                    i++;
+
+                }
+                Console.WriteLine("Resultado: " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("Resultado: " + resultado);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Resultado: " + resultado);
+        }
+
+        Console.ReadKey();
+    }
+}
