142aa76781804f7f9c649dec4d38752321fa607b
masanchezvar <142190930+masanchezvar@users.noreply.github.com>
2023-11-06 09:11:23 -0600

    Add files via upload

---
+++ SEMANA14_LABORATORIO_1055323_ALEXANDRA SÁNCHEZ/L14MASV1055323/L14MASV1055323/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+class program
+{
+    static void Main(string[] args)
+    {
+
+        int numero = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Ingrese un número entero:");
+            Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Los números ingresados fueron: " + numero );
+        }
+
+
+
+
+
+
+        Console.ReadKey();
+    }
+}
